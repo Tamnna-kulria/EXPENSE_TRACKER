a29ac8749f28c04171ba8142c35c0cf6d2e0d96e
tamnnakulria <kulriatamnna@gmail.com>
2026-05-03 01:06:43 +0530

    Update BASE_URL to new endpoint

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL ="https://expense-tracker-jdft.onrender.com";
+export const BASE_URL ="https://expense-tracker-2-cwha.onrender.com";
 
 //utils/apiPath.js
 export const API_PATHS = {
